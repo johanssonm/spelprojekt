7df1e7f948ce15d7eeb38c70ea253a742306a74a
Unknown <mattiasj@protonmail.com>
2018-08-21 13:13:42 +0200

    Revert "Commit innan migration"
    This reverts commit 0374c22516742e49b7fc13f3f9e3945088cdc1e8.

--- Spelprojekt/Entities/Player.cs
+++ Spelprojekt/Entities/Player.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace Spelprojekt.Entities
+﻿namespace Spelprojekt.Entities
 {
     public class Player
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Score> Scores { get; set; }
     }
 }
--- Spelprojekt/Entities/Score.cs
+++ Spelprojekt/Entities/Score.cs
@@ -4,5 +4,6 @@
     {
         public int Id { get; set; }
         public int ScoreAmount { get; set; }
+        public int PlayerId { get; set; }
     }
 }
